b0b385872069da91980ca77d42c58da35e56ead2
NiTzuA <11v35faustin@gmail.com>
2025-07-24 12:37:32 +0800

    Added Text Typing Animation
    This commit includes the addition of a typewriter styled animation and optimized text scatter animation

--- world-execute-me/world-execute-me/Program.cs
+++ world-execute-me/world-execute-me/Program.cs
@@ -1,23 +1,59 @@
 ﻿using System.Diagnostics;
 
 Random ran = new Random();
-TextAnimationRandomized(5, "Hello World!", 100);
+// TextAnimationRandomized(2, "Hello World!", 100);
+TextTypingAnimation("Hello there, ", 0.5, false);
+TextScatterAnimation("Austin", 1, 10, true);
 
-void TextAnimationRandomized(int duration, string text, int animationSpeed)
+void TextScatterAnimation(string text, int duration, int animationSpeed, bool highlight)
 {
     int stringLength = text.Length;
     Stopwatch stopwatch = Stopwatch.StartNew();
 
     while (stopwatch.Elapsed.TotalSeconds < duration) // duration is in seconds, animationSpeed is in miliseconds
     {
-        RandomAsciiString(stringLength);
-        Console.Write("\r" + RandomAsciiString(stringLength));
+        Console.Write(RandomAsciiString(stringLength));
         Thread.Sleep(animationSpeed);
+        Console.SetCursorPosition(Console.CursorLeft - stringLength, Console.CursorTop);
     }
 
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.Write("\r" + text);
-    Console.ForegroundColor = ConsoleColor.White;
+    Console.CursorVisible = true;
+
+    if (highlight)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(text);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+void TextTypingAnimation(string text, double duration, bool highlight)
+{
+    int durationDivisor = 1;
+    if (highlight)
+    {
+        durationDivisor = text.Length + 1;
+    } else
+    {
+        durationDivisor = text.Length;
+    }
+
+    double animationSpeed = duration / durationDivisor;
+    for (int i = 0; i < text.Length; i++)
+    {
+        Console.Write(text[i]);
+
+        Thread.Sleep((int)(animationSpeed*1000));
+    }
+
+    Thread.Sleep((int)(animationSpeed * 1000));
+
+    if (highlight)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("\r" + text);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
 }
 
 string RandomAsciiString(int length)
