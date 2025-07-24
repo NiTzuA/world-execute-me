a7b22983999cf6757217d6cf1cf703907e892f8d
NiTzuA <11v35faustin@gmail.com>
2025-07-24 11:42:48 +0800

    Created Text Scramble Animation
    This commit includes the creation of a text scramble animation which accepts duration, the text, and the speed of the animation.

--- world-execute-me/world-execute-me/Program.cs
+++ world-execute-me/world-execute-me/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Random ran = new Random();
+TextAnimationRandomized(5, "Hello World!", 100);
+
+void TextAnimationRandomized(int duration, string text, int animationSpeed)
+{
+    int stringLength = text.Length;
+    Stopwatch stopwatch = Stopwatch.StartNew();
+
+    while (stopwatch.Elapsed.TotalSeconds < duration) // duration is in seconds, animationSpeed is in miliseconds
+    {
+        RandomAsciiString(stringLength);
+        Console.Write("\r" + RandomAsciiString(stringLength));
+        Thread.Sleep(animationSpeed);
+    }
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("\r" + text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+string RandomAsciiString(int length)
+{
+    char[] chars = new char[length];
+    for (int i = 0; i < length; i++)
+    {
+        chars[i] = (char)ran.Next(32, 127);
+    }
+    return new string(chars);
+}
